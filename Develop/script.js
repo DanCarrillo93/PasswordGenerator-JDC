602a55bef4f82160d58c07393cbfe99eae372721
Jose Daniel Carrillo <danielcp_67@hotmail.com>
2021-03-18 17:46:52 -0700

    Added prompt and condition to have right number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,3 @@
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var nmrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var spclC = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-var nCharacters = window.prompt("Number of characters to include:");
-console.log(nCharacters);
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -20,3 +12,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Possible characters to appear on the password
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var nmrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var spclC = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+//
+var nCharacters = window.prompt("Number of characters to include (At least 8 and maximum 128):");
+//console.log(nCharacters);
+
+
+if(nCharacters >= 8 && nCharacters < 129) {
+  console.log(nCharacters);
+} else {
+  alert("Incorrect number of characters or invalid InputDeviceInfo.");
+  var nCharacters = window.prompt("Number of characters to include (At least 8 and maximum 128):");
+}
